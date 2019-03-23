57955841117f88055ef75e302f022da89ec4b5ae
Paulo Tarcisio <paulo_tarcisio93@hotmail.com>
2019-03-23 15:08:17 -0300

    Procfile added

---
+++ src/about/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet con</p>
+
+        <h2>Missão</h2>
+        <p>Lorem ipsum dolor sit amet con</p>
+       
+        <h2>Valores</h2>
+        <p>Lorem ipsum dolor sit amet con</p>
+
+        
+    </div>
+)
---
+++ src/consts.js
@@ -0,0 +1,3 @@
+export default {
+    API_URL: 'https://todo-app-back-by-pqdt.herokuapp.com/api/todos'
+}
